2b1fe002fed240ff18229dc8fed41ae47a7e905c
Kirill Laryshkin <larka69rus@gmail.com>
2025-08-11 16:22:53 +0300

    I fixed the code to PEP8 standards

--- Количество уникальных значений.py
+++ Количество уникальных значений.py
@@ -1,9 +1,11 @@
 
-numbers = list(map(int, input().split()))
-new_list = []
-for x in numbers:
-    if x  not in new_list:
-        new_list.append(x)
+list_numbers = list(map(int, input().split()))
 
-s = len(new_list)
-print(s)
+list_unique_numbers = []
+
+for number in list_numbers :
+    if number not in list_unique_numbers :
+       list_unique_numbers.append(number)
+
+print(len(list_unique_numbers))
+
