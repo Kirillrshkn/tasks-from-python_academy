fa5fbed7e55c460717c353e5dedf065c7e7ae8c5
Kirill Laryshkin <larka69rus@gmail.com>
2025-08-11 16:08:32 +0300

    I Fixed the code to PEP8 standards

--- the_product_of_numbers_in_list.py
+++ the_product_of_numbers_in_list.py
@@ -1,5 +1,7 @@
 import math
 
-numbers = list(map(int, input().split()))
-s= math.prod(numbers)
-print(s)
+list_numbers = list(map(int,input().split()))
+
+the_product_of_numbers = math.prod(list_numbers)
+
+print(the_product_of_numbers)
